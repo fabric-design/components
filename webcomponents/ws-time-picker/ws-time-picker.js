d009c98cfc926fc161dc9e1abb063e34aff3812b
Oleksandr Kukhtin <okukhtin@zalando-20906.corp.ad.zalando.net>
2016-06-07 13:54:15 +0200

    updates js

--- webcomponents/ws-time-picker/ws-time-picker.js
+++ webcomponents/ws-time-picker/ws-time-picker.js
@@ -1,6 +1,6 @@
 //Get the contents of the template (_currentScript is available with webcomponents.js, use currentScript if you don't use this Polyfill)
 var template = document.currentScript.ownerDocument.querySelector('template');
-var input, div;
+var input, div, hoursInput, minutesInput, hourUpButton, hourDownButton, minuteUpButton, minuteDownButton;
 
 class WSTimePicker extends HTMLElement {
   createdCallback() {
@@ -8,11 +8,44 @@ class WSTimePicker extends HTMLElement {
     this.createShadowRoot().appendChild(clone);
     input = this.shadowRoot.querySelector('.ws-time-picker');
     div = this.shadowRoot.querySelector('.time-picker');
+    hoursInput = this.shadowRoot.querySelector('.hours');
+    minutesInput = this.shadowRoot.querySelector('.minutes');
+    hourUpButton = this.shadowRoot.querySelector('.hourUp');
+    hourDownButton = this.shadowRoot.querySelector('.hourDown');
+    minuteUpButton = this.shadowRoot.querySelector('.minuteUp');
+    minuteDownButton = this.shadowRoot.querySelector('.minuteDown');
     this.shadowRoot.addEventListener('click', this.openTimePicker);
+    hourUpButton.addEventListener('click', this.hourUp);
+    hourDownButton.addEventListener('click', this.hourDown);
+    minuteUpButton.addEventListener('click', this.minuteUp);
+    minuteDownButton.addEventListener('click', this.minuteDown);
   }
   
   openTimePicker() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    hoursInput.value = hours < 10 ? '0' + hours : hours;
+    minutesInput.value = minutes < 10 ? '0' + minutes : minutes;
     div.className += ' opened';
+  }
+
+  hourUp() {
+    console.log(hoursInput.value);
+    hoursInput.setAttribute('value', '20');
+    console.log(hoursInput);
+  }
+
+  hourDown() {
+    hoursInput.value = Number(hoursInput.value) - 1;
+  }
+
+  minuteUp() {
+    minutesInput.value += 5;
+  }
+
+  minuteDown() {
+    hoursInput.value = Number(minutesInput.value) - 5;
   }
 }
 

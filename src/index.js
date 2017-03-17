7536cf891a8fa6dbed2f586e3386366d4969dac4
Ferit Topcu <ferit.topcu@zalando.de>
2017-03-17 14:59:27 +0100

    Updating reference of index start page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 /**
+ * Our Wholesale-Styleguide Component documentation
+ * 
  * This is our Startpage for our component API
  * based on jsDocs
  * Here we should add more descriptions
- * Below are all components referenced
+ * Below are all components referenced 
+ * See {@link ws-header/ws-header.md|WSHeader}
+ * See {@link ws-date-picker/ws-date-picker.md|WSDatePicker}
+ * See {@link ws-dropdown/ws-dropdown.md|WSDropdown}
  * @module WHStyleguide
- * @see WSHeader {@link ws-header/ws-header.md| WSHeader}
- * @see WSDatePicker {@link ws-date-picker/ws-date-picker.md|WSDatePicker}
- * @see WSDropdown {@link ws-dropdown/ws-dropdown.md|WSDropdown}
  */
 
 export {WSHeader} from './ws-header/ws-header';

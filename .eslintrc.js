6894d057bd65488dea5d2c616bffbd766f927ed1
Fabian Weber <frznhope@gmail.com>
2017-03-28 10:34:38 +0200

    fixed the paths of the components in the linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,6 +47,7 @@ module.exports = {
     "jsx-a11y/mouse-events-have-key-events": 2,
     "jsx-a11y/role-has-required-aria-props": 2,
     "jsx-a11y/role-supports-aria-props": 2,
+    "jsx-a11y/no-static-element-interactions": 0,
     "max-len": 0,
     "newline-per-chained-call": 0,
     "no-console": 2,

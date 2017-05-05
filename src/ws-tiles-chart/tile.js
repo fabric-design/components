83f67ad97dc9b241c4590896523717e861b102b4
Inanda Menezes <inanda.menezes@zalando.de>
2017-05-05 18:42:59 +0200

    Fix lints and improve readability

--- src/ws-tiles-chart/tile.js
+++ src/ws-tiles-chart/tile.js
@@ -1,21 +1,48 @@
-import {Component, PropTypes} from "../imports";
+import React from 'react';
+import {Component, PropTypes} from '../imports';
 
+/**
+ * This class describes a Preact/React component which renders a single tile
+ * @property {Object} props React properties object
+ * @property {Object} props.config Defines the configuration of the tile, e.g. the background color
+ * @property {number} props.size Defines the width and height of the tile
+ * @returns {string} HTML markup for the component
+ */
+export class Tile extends Component {
 
-
-export const Tile = ({data, config, size}) => {
-  const style = {
-    'backgroundColor': config.backgroundColor,
-    width: `${size}px`,
-    height: `${size}px`
+  /**
+   * @type {Object}
+   */
+  static propTypes = {
+    data: PropTypes.object,
+    config: PropTypes.object,
+    size: PropTypes.number
   };
 
-  return (
-    <div key={data.key} className="tile" style={style}/>
-  );
-};
 
-Tile.propTypes = {
-  data: PropTypes.object,
-  config: PropTypes.object,
-  size: PropTypes.number
+  /**
+   * @type {Object}
+   */
+  static defaultProps = {
+    data: {},
+    config: {},
+    size: 25
+  };
+
+  /**
+   * Renders the chart
+   * @returns {Object}
+   */
+  render() {
+    const {config, size} = this.props;
+    const style = {
+      backgroundColor: config.backgroundColor,
+      width: `${size}px`,
+      height: `${size}px`
+    };
+
+    return (
+      <div className="tile" style={style} />
+    );
+  }
 }
--- src/ws-tiles-chart/ws-tiles-chart.js
+++ src/ws-tiles-chart/ws-tiles-chart.js
@@ -1,10 +1,16 @@
-import {Component, PropTypes} from "../imports";
-import {Tile} from "./tile";
+import React from 'react';
+import {Component, PropTypes} from '../imports';
+import {Tile} from './tile';
 
 /**
  * This class describes a Preact/React component which renders a tiles chart
  * @class WSTilesChart
  * @property {Object} props React properties object
+ * @property {Object} props.data Defines the Groups of tiles that should be shown in the chart
+ * @property {Object} props.config Defines the configuration of the component, e.g. the color of each group of tiles
+ * @property {string} props.title Title of the chart
+ * @property {number} props.maxTileSize Defines the maximum size that the tile can be
+ *
  */
 export class WSTilesChart extends Component {
 
@@ -36,32 +42,46 @@ export class WSTilesChart extends Component {
     super(props);
     this.state = {tileSize: 0};
 
-    this.calculateMaximumPossibleTileSize = this.calculateMaximumPossibleTileSize.bind(
-      this);
+    this.calculateMaximumPossibleTileSize = this.calculateMaximumPossibleTileSize.bind(this);
   }
 
+  /**
+   * Calculate the tiles size
+   * @returns {void}
+   */
   componentDidMount() {
-    //Calculate the tiles size
-    const tilesAmount = this.props.data.groups.map(group=> {
-      return group.tilesSet.length
-    }).reduce((a, b)=>a + b);
+    const tilesAmount = this.props.data.groups.map(group => group.tilesSet.length).reduce((a, b) => a + b);
 
     const tileSize = this.calculateMaximumPossibleTileSize(this.tilesChartContainer.clientWidth,
       this.tilesChartContainer.clientHeight, tilesAmount);
     this.setState({
-      tileSize: (tileSize<this.props.maxTileSize)? tileSize:this.props.maxTileSize
+      tileSize: (tileSize < this.props.maxTileSize) ? tileSize : this.props.maxTileSize
     });
   }
 
   /**
+   * Finds the config of the group
+   * @param {array} config Array of group configurations
+   * @param {object} group to be filtered
+   * @returns {object}
+   */
+  getGroupConfig(config, group) {
+    if (config.groupConfigs) {
+      return config.groupConfigs.find(groupConfig => groupConfig.key === group.key);
+    }
+    return {};
+  }
+
+  /**
    * Calculates the maximum tile size based on total width, height and amount of tiles
-   * @param {width} chart container width
-   * @param {height} chart container height
+   * @param {number} width of the container
+   * @param {number} height of the container
+   * @param {number} tilesAmount total number of tiles in the chart
    * @returns {int}
    */
   calculateMaximumPossibleTileSize(width, height, tilesAmount) {
     const chartArea = (width < height) ? width * width : height * height;
-    return Math.sqrt((chartArea) / tilesAmount)
+    return Math.sqrt(chartArea / tilesAmount);
   }
 
   /**
@@ -73,20 +93,18 @@ export class WSTilesChart extends Component {
     return (
       <div className="ws-tiles-chart">
         <div className="tiles-chart-title">{title}</div>
-
-        <div className="tiles-chart-container"
-             ref={(element)=>this.tilesChartContainer=element}>
-          {data.groups.map(group=>group.tilesSet.map(
-            tile => {
-              const groupConfig = config.groupConfigs
-                ? config.groupConfigs.find(config=>config.key === group.key)
-                : {};
-              return <Tile data={tile} config={groupConfig}
-                           size={this.state.tileSize}/>
-            }
+        <div
+          className="tiles-chart-container"
+          ref={element => { this.tilesChartContainer = element; }}
+        >
+          {data.groups.map(group => group.tilesSet.map(tile =>
+            <Tile
+              data={tile}
+              config={this.getGroupConfig(config, group)}
+              size={this.state.tileSize}
+            />
           ))}
         </div>
-
       </div>
     );
   }

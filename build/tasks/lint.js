45736498cf762366ebd49e0f1d4ccd7c3f64e0da
Thomas Schlage <thomas.schlage@zalando.de>
2017-06-07 13:49:56 +0200

    Moved document linting into separate task

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -1,12 +1,20 @@
-var gulp = require('gulp');
-var paths = require('../paths');
-var eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const paths = require('../paths');
+const eslint = require('gulp-eslint');
+const runSequence = require('run-sequence');
 const glob = require('glob');
 const docjs = require('documentation');
 const REGEX_FILE_NAME = /((\w)+(-)*(\w))+(?=.js)/g;
 
 // runs eslint on all .js files
-gulp.task('lint', function() {
+gulp.task('eslint', () =>
+  gulp.src([paths.source, paths.tests])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError())
+);
+
+gulp.task('doc-lint', () =>
   Promise.resolve(glob.sync(paths.source))
     // Filter file names to match tag name pattern
     .then(fileNames => fileNames.filter(fileName => {
@@ -14,14 +22,14 @@ gulp.task('lint', function() {
       return name !== 'imports';
     }))
     // Generate markdown for each file
-    .then(fileNames => fileNames.forEach(fileName =>
+    .then(fileNames => fileNames.map(fileName =>
       docjs.lint(fileName, {}).then(lintOutput => {
         console.log(lintOutput);
       })
-    ));
+    ))
+    .then(promises => Promise.all(promises))
+);
 
-  return gulp.src([paths.source, paths.tests])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
-});
+gulp.task('lint', done =>
+  runSequence(['eslint', 'doc-lint'], done)
+);

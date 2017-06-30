0df8723d855fb89b3ac4ccdb57014fe08fa99c6d
Fabian Weber <frznhope@gmail.com>
2017-06-30 13:14:49 +0200

    fix(week-picker): linting

--- tests/async.js
+++ tests/async.js
@@ -1,6 +1,7 @@
 /**
  * Helper that returns a promise which resolves after 10ms or the time passed as an argument
  * @param {number} timeout time to wait until resolving promise
+ * @returns {Promise}
  */
 export function wait(timeout = 10) {
   const promise = new Promise(resolve => {

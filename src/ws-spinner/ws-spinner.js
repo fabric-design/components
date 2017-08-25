affe21ca300bd0fec808a3d7b714d2a62d2a4f81
Thomas Schlage <fragsalat@users.noreply.github.com>
2017-08-25 13:23:22 +0200

    feat(spinner): Created spinner component (#115)
    * feat(spinner): Created spinner component and introduced a index.scss to include all component styles
    
    * Fixed spinner color

---
+++ src/ws-spinner/ws-spinner.js
@@ -0,0 +1,43 @@
+import {React, Component, PropTypes} from '../imports';
+
+/**
+ * Renders a simple spinner with the primary color
+ * @property {string} size One of small, medium, large. Default to medium
+ * @property {boolean} isWhite True if the spinner color should be white
+ */
+export class WSSpinner extends Component {
+
+  /**
+   * @type {Object}
+   */
+  static propTypes = {
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    isWhite: PropTypes.bool
+  };
+
+  /**
+   * @type {Object}
+   */
+  static defaultProps = {
+    size: 'medium',
+    isWhite: false
+  };
+
+  /**
+   * @returns {XML}
+   */
+  render() {
+    return (
+      <div className={`spinner mod-${this.props.size} ${this.props.isWhite ? 'mod-white' : ''}`}>
+        <div className="spinner-layer">
+          <div className="circle-clipper left">
+            <div className="circle" />
+          </div>
+          <div className="circle-clipper right">
+            <div className="circle" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

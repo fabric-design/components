6ae40efa6528361cb449a79e9202b6df11f0ebf4
Mouse Braun <jason.joseph.braun@zalando.de>
2018-08-22 10:38:32 +0200

    fix(webpack) removes unused components from the bundle

--- webpack-loaders/index.js
+++ webpack-loaders/index.js
@@ -15,24 +15,12 @@
 
 import { WSHeader } from '../src/ws-header/ws-header';
 import { WSDatePicker } from '../src/ws-date-picker/ws-date-picker';
-import { WSDropdown } from '../src/ws-dropdown/ws-dropdown';
-import { WSInlineEdit } from '../src/ws-inline-edit/ws-inline-edit';
 import { WSNotification } from '../src/ws-notification/ws-notification';
-import { WSWeekPicker } from '../src/ws-week-picker/ws-week-picker';
-import { WSTilesChart } from '../src/ws-tiles-chart/ws-tiles-chart';
 import { WSSpinner } from '../src/ws-spinner/ws-spinner';
-import { WSTabMenu } from '../src/ws-tab-menu/ws-tab-menu';
-import { WSOptionButtons } from '../src/ws-option-buttons/ws-option-buttons';
 
 export default {
   WSHeader,
   WSDatePicker,
-  WSDropdown,
-  WSInlineEdit,
   WSNotification,
-  WSWeekPicker,
-  WSTilesChart,
   WSSpinner,
-  WSTabMenu,
-  WSOptionButtons,
 };
